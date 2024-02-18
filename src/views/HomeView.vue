7b9813ac0385c3dd76fb36ae956f7ac68fd0d705
Maksim Kropanov <kropanov2012@gmail.com>
2024-02-18 16:53:09 +0300

    Remove useless files & Refactor code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+export default {
+  components: { HelloWorld },
+};
 </script>
