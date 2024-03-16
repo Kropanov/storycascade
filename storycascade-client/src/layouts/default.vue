dcef22243e98e80ebb1a1d3afc8021b75b6eeaf7
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2024-03-16 21:46:54 +0300

    Upgrade to v3 (#3)
    * Fixes
    
    - Rework component names
    - Add scss dir
    - Append a new view
    
    * Create grid for editor view component
    
    * Project upgrade
    
    - vue3
    - vuetify
    - pinia
    
    * Remove .idea folder
    
    * Fixes
    
    - Add AppBar.vue
    - Delete HelloWorld.vue
    - Configure prettier

---
+++ storycascade-client/src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <AppBar />
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+import AppFooter from "@/components/AppFooter.vue";
+import AppBar from "@/components/AppBar.vue";
+</script>
---
+++ storycascade-client/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>home</div>
+</template>
+
+<script setup>
+//
+</script>
