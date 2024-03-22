cd31d73e0231fc5657c4d20bfe9d3736d81d0032
Maksim Kropanov <kropanov2012@gmail.com>
2024-03-23 00:45:32 +0300

    Fixes
    - Append rules to eslint
    - Create text field with grid on the index page

--- storycascade-client/src/pages/index.vue
+++ storycascade-client/src/pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>home</div>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col lg="10" xl="8">
+        <v-text-field
+          label="Search"
+          variant="solo"
+          :loading="loading"
+          append-inner-icon="mdi-magnify"
+          @click:append-inner="onClick"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue';
+
+const loading = ref(false);
+
+function onClick() {
+  loading.value = true;
+}
 </script>
+
+<style scoped></style>
