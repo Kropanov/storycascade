a0768259a51cf6c668a6f0156544edabbf6d9ad8
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2024-03-26 21:40:26 +0300

    Upgrade Nest.js to typescript (#7)
    - Upgrade nest js from javascript to typescript
    - Put a new port
    - Getting cards data from th server
    - Create a new util fetch.js
    - Add proxy
    - Append a new global prefix - /api/v1

---
+++ storycascade-client/src/util/fetch.js
@@ -0,0 +1,24 @@
+// fetch.js
+import { ref, watchEffect, toValue } from 'vue';
+
+export function useFetch(url) {
+  const data = ref(null);
+  const error = ref(null);
+
+  watchEffect(async () => {
+    // reset state before fetching..
+    data.value = null;
+    error.value = null;
+
+    const urlValue = toValue(url);
+
+    try {
+      const res = await fetch('/api/v1' + urlValue);
+      data.value = await res.json();
+    } catch (e) {
+      error.value = e;
+    }
+  });
+
+  return { data, error };
+}
