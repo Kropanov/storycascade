83294b125378122d4eb3b359eefbaafbb53aa5b7
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-16 22:21:36 +0300

    fix: change a value of error color in custom theme

--- storycascade-client/src/plugins/vuetify.js
+++ storycascade-client/src/plugins/vuetify.js
@@ -23,7 +23,7 @@ const custom = {
     'primary-darken-1': '#1F5592',
     secondary: '#48A9A6',
     'secondary-darken-1': '#018786',
-    error: '#B00020',
+    error: '#FFA500',
     info: '#2196F3',
     success: '#4CAF50',
     warning: '#FB8C00',
