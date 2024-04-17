83b5adbe714321a9db74da8f5f42c0005399abef
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-17 19:03:27 +0300

    feat: create a new layout of profile page

---
+++ storycascade-client/src/components/ProfileHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex justify-center mb-3">
+    <v-card width="300px">
+      <v-img
+        cover
+        height="256"
+        max-width="256"
+        class="rounded-circle mx-auto mt-4"
+        aspect-ratio="16/9"
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+      />
+      <v-card-title class="text-h6 text-md-h5 text-lg-h4">User name</v-card-title>
+      <v-card-text> Description </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
