cecec4e8e0ed9c17a039f371973156107588f4ba
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-18 18:22:44 +0300

    fix: change profile layout

--- storycascade-client/src/components/ProfileTabs.vue
+++ storycascade-client/src/components/ProfileTabs.vue
@@ -1,15 +1,90 @@
 <template>
   <v-tabs v-model="tab" align-tabs="start">
-    <v-tab :value="1">Favorite</v-tab>
-    <v-tab :value="2">Novels</v-tab>
-    <v-tab :value="3">Settings</v-tab>
+    <v-tab value="1">Favorite</v-tab>
+    <v-tab value="2">Novels</v-tab>
+    <v-tab value="3">Settings</v-tab>
   </v-tabs>
+  <v-window v-model="tab">
+    <v-window-item value="1">
+      <v-card variant="text">
+        <v-card-text>
+          <p>
+            Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus.
+            Sed a libero.
+          </p>
+
+          <p>
+            Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed,
+            posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit
+            hendrerit rutrum.
+          </p>
+
+          <p class="mb-0">
+            Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et,
+            tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-window-item>
+    <v-window-item value="2">
+      <v-card variant="text">
+        <v-card-text>
+          <p>
+            Morbi nec metus. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae
+            iaculis lacus elit id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non
+            adipiscing dolor urna a orci. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo.
+            Nunc sed turpis.
+          </p>
+
+          <p>
+            Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae
+            iaculis lacus elit id tortor. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien,
+            a accumsan nisi mauris ac eros. In hac habitasse platea dictumst. Fusce ac felis sit amet ligula pharetra
+            condimentum.
+          </p>
+
+          <p>
+            Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Nam
+            commodo suscipit quam. In consectetuer turpis ut velit. Sed cursus turpis vitae tortor. Aliquam eu nunc.
+          </p>
+
+          <p>
+            Etiam ut purus mattis mauris sodales aliquam. Ut varius tincidunt libero. Aenean viverra rhoncus pede. Duis
+            leo. Fusce fermentum odio nec arcu.
+          </p>
+
+          <p class="mb-0">
+            Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In dui magna, posuere eget, vestibulum et,
+            tempor auctor, justo. Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed, iaculis a,
+            condimentum nec, nisi.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-window-item>
+    <v-window-item value="3">
+      <v-card variant="text">
+        <v-card-text>
+          <p>
+            Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit
+            amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere
+            eget, vestibulum et, tempor auctor, justo.
+          </p>
+
+          <p class="mb-0">
+            Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis,
+            euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus
+            interdum sagittis.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-window-item>
+  </v-window>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const tab = ref(null);
+const tab = ref(1);
 </script>
 
 <style scoped></style>
