6210b4670ba520c375f4c3043eaac041d69e4818
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-18 17:54:01 +0300

    fix: rename table

--- storycascade-server/src/novels/novels.service.ts
+++ storycascade-server/src/novels/novels.service.ts
@@ -14,32 +14,32 @@ export class NovelsService {
   async create(createNovelDto: CreateNovelDto) {
     const { id, title, image, description, rating, genre } = createNovelDto;
     const res = await this.postgres.query(
-      'INSERT INTO novel (id, title, image, description, rating, genre) VALUES ($1, $2, $3, $4, $5, $6)',
+      'INSERT INTO novels (id, title, image, description, rating, genre) VALUES ($1, $2, $3, $4, $5, $6)',
       [id, title, image, description, rating, genre],
     );
     return res.rows;
   }
 
   async findAll() {
-    const res = await this.postgres.query('SELECT * FROM novel', []);
+    const res = await this.postgres.query('SELECT * FROM novels', []);
     return res.rows;
   }
 
   async findOne(id: number) {
-    const res = await this.postgres.query('SELECT * FROM novel WHERE id = $1', [id]);
+    const res = await this.postgres.query('SELECT * FROM novels WHERE id = $1', [id]);
     return res.rows[0];
   }
 
   async update(_id: number, updateNovelDto: UpdateNovelDto) {
     const { id, title, image, description, rating, genre } = updateNovelDto;
     return await this.postgres.query(
-      'UPDATE novel SET id = $1, title = $2, image = $3, description = $4, rating = $5, genre = $6 WHERE id = $7',
+      'UPDATE novels SET id = $1, title = $2, image = $3, description = $4, rating = $5, genre = $6 WHERE id = $7',
       [id, title, image, description, rating, genre, _id],
     );
   }
 
   async remove(id: number) {
-    const res = await this.postgres.query('DELETE FROM novel WHERE id = $1', [id]);
+    const res = await this.postgres.query('DELETE FROM novels WHERE id = $1', [id]);
     return res.rows;
   }
 }
