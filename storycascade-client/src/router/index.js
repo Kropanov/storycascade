c0434e7a2af5dce1140cbc6d3aaa670e3fb0495b
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-23 20:37:38 +0300

    fix: remove wrong way to work with routes in the app

--- storycascade-client/src/router/index.js
+++ storycascade-client/src/router/index.js
@@ -8,18 +8,9 @@
 import { createRouter, createWebHistory } from 'vue-router/auto';
 import { setupLayouts } from 'virtual:generated-layouts';
 
-import index from '@/pages/index.vue';
-import profile from '@/pages/profile.vue';
-
-const routes = [
-  { path: '/', component: index },
-  { path: '/profile', component: profile },
-];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   extendRoutes: setupLayouts,
-  routes,
 });
 
 export default router;
