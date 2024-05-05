f7f970e672ac7813d47989963421d35cb6fc6642
Maksim Kropanov <kropanov2012@gmail.com>
2024-05-05 20:30:33 +0300

    chore(sql): fix naming of sql files

--- storycascade-server/src/db-migrations/sql/create_comments_table_05.sql
+++ storycascade-server/src/db-migrations/sql/create_comments_table_05.sql
@@ -1,26 +1,24 @@
-DROP TABLE IF EXISTS novels;
+DROP TABLE IF EXISTS comments;
 
-CREATE TABLE IF NOT EXISTS novels (
+CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    authors VARCHAR(100) NOT NULL,
-    description VARCHAR(2000) NOT NULL,
-    country varchar(50),
-
+    content VARCHAR(1200) NOT NULL,
+    user_id int NOT NULL REFERENCES users(id),
+    novels_id int NOT NULL REFERENCES novels(id),
+    parent_comment_id int REFERENCES comments(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 CREATE OR REPLACE FUNCTION update_timestamp()
-    RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
+RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_users_timestamp
-    BEFORE UPDATE ON novels
+    BEFORE UPDATE ON comments
     FOR EACH ROW
     EXECUTE FUNCTION update_timestamp();
