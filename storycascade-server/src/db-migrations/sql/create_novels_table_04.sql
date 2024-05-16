3c3cf2beeb94db2a378202e2bd5ce773b79bd7e3
Maksim Kropanov <kropanov2012@gmail.com>
2024-05-16 19:54:06 +0300

    chore(sql): update a sql file

--- storycascade-server/src/db-migrations/sql/create_novels_table_04.sql
+++ storycascade-server/src/db-migrations/sql/create_novels_table_04.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS novels;
 
 CREATE TABLE IF NOT EXISTS novels (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL UNIQUE,
+    title VARCHAR(150) NOT NULL UNIQUE,
+    original_title VARCHAR(250),
     description VARCHAR(2000) NOT NULL,
     chapters INT NOT NULL,
     status_id int NOT NULL REFERENCES novel_status(id)
