217d3a229866b0d92d912fe38f4daefbd9f11af6
Maksim Kropanov <kropanov2012@gmail.com>
2024-05-21 21:23:29 +0300

    refactor: the novel list UI

--- storycascade-client/src/components/NovelList.vue
+++ storycascade-client/src/components/NovelList.vue
@@ -1,14 +1,14 @@
 <template>
   <v-card class="mx-auto">
-    <v-container fluid>
+    <v-container v-if="!novels" fluid>
       <v-row v-if="loading">
         <v-col v-for="n in 6" :key="n" cols="12" xxl="1" xl="2" lg="2" md="4" sm="6">
-          <v-skeleton-loader height="250" type="card"></v-skeleton-loader>
+          <v-skeleton-loader height="250" type="novel"></v-skeleton-loader>
         </v-col>
       </v-row>
       <v-row v-else>
-        <v-col v-for="card in cards" :key="card.title" cols="12" xxl="1" xl="2" lg="2" md="4" sm="6">
-          <NovelListItem @click="() => onClickNovel(card)" :card="card" />
+        <v-col v-for="novel in novels" :key="novel.title" cols="12" xxl="1" xl="2" lg="2" md="4" sm="6">
+          <NovelListItem @click="() => onClickNovel(novel)" :novel="novel" />
         </v-col>
       </v-row>
     </v-container>
@@ -22,7 +22,7 @@ import { useFetch } from '@/utils/fetch';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const cards = ref([]);
+const novels = ref([]);
 const loading = ref(false);
 const url = ref('/novels');
 
@@ -32,7 +32,7 @@ const { data } = useFetch(url);
 
 watchEffect(() => {
   if (data.value !== null) {
-    cards.value = data.value;
+    novels.value = data.value;
     loading.value = false;
   }
 });
