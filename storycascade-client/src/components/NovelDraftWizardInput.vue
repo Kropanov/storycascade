302114918a1b9c33b47d52fafa388d8a73de7499
Maksim Kropanov <kropanov2012@gmail.com>
2024-05-25 20:28:35 +0300

    feat: add some functionality to draft input

--- storycascade-client/src/components/NovelDraftWizardInput.vue
+++ storycascade-client/src/components/NovelDraftWizardInput.vue
@@ -1,10 +1,41 @@
 <template>
   <div class="input-area">
-    <v-text-field variant="outlined"></v-text-field>
+    <v-text-field
+      class="mb-2"
+      append-inner-icon="mdi-send"
+      prepend-inner-icon="mdi-paperclip"
+      density="comfortable"
+      label="Enter..."
+      variant="solo"
+      hide-details
+      single-line
+      @click:append-inner="onClickSendMessage"
+      @click:prepend-inner="onClickUploadFile"
+    ></v-text-field>
+    <input style="display: none" type="file" ref="fileInput" @change="onFileSelected" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const fileInput = ref(null);
+
+const onClickSendMessage = () => {
+  console.log('Sent Message!');
+};
+
+const onClickUploadFile = () => {
+  fileInput.value.click();
+};
+
+const onFileSelected = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    console.log(`Selected file: ${file.name}`);
+  }
+};
+</script>
 
 <style scoped>
 .input-area {
