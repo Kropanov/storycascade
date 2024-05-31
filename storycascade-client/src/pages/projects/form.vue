e6edc155acf3107a9325975101d5a0164d9be51e
Maksim Kropanov <kropanov2012@gmail.com>
2024-05-31 18:31:24 +0300

    fix: add a new way for creating novel by using a form

---
+++ storycascade-client/src/pages/projects/form.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Form</h1>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- storycascade-client/src/pages/projects/index.vue
+++ storycascade-client/src/pages/projects/index.vue
@@ -1,14 +1,35 @@
 <template>
-  <v-btn class="ma-3" text="Create draft" variant="outlined" size="large" @click="onDraftClick"></v-btn>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" class="ma-3" text="Create draft" variant="outlined" size="large"></v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in items"
+        @click="onClickItem(item.path)"
+        :append-icon="item.icon"
+        :key="index"
+        :value="index"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const onDraftClick = () => {
-  router.push({ path: 'projects/draft' });
+const items = ref([
+  { title: 'Form', icon: 'mdi-ballot', path: 'projects/form' },
+  { title: 'Wizard', icon: 'mdi-wizard-hat', path: 'projects/wizard' },
+]);
+
+const onClickItem = (path) => {
+  router.push({ path });
 };
 </script>
 
