8be2892c2658d7ec54bfc578c8dcef4371d917ab
Maksim Kropanov <kropanov2012@gmail.com>
2024-06-29 15:35:04 +0300

    styles(UI): reformat code with prettier and eslint

--- storycascade-client/src/plugins/index.js
+++ storycascade-client/src/plugins/index.js
@@ -5,13 +5,10 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import pinia from '@/stores'
-import router from '@/router'
+import vuetify from './vuetify';
+import pinia from '@/stores';
+import router from '@/router';
 
-export function registerPlugins (app) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app) {
+  app.use(vuetify).use(router).use(pinia);
 }
